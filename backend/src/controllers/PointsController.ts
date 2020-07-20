8e8f745e452d376327b6fd6d1b85746e08a36899
lorenzowind <lorenzomart01@gmail.com>
2020-07-20 17:30:09 -0400

    feat: specific point listing route implementation

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -3,6 +3,22 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async show(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const point = await knex('points').where('id', id).first();
+
+    if (!point) {
+      return response.status(400).json({ message: 'Point not found.' });
+    }
+
+    const items = await knex('items')
+      .join('point_item', 'items.id', '=', 'point_item.item_id')
+      .where('point_item.point_id', id);
+
+    return response.json({ point, items });
+  };
+  
   async create(request: Request, response: Response) {
     const {
       name,
